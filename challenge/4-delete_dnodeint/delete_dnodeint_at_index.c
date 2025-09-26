e8302f27931a631c85867d45c919813f3773d6b2
razan <razan_8801@icloud.com>
2025-09-26 23:51:23 +0000

    update  task 4

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -12,7 +12,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
     unsigned int i = 0;
 
     if (!head || !*head)
-        return -1;
+        return (-1);
 
     while (node && i < index)
     {
@@ -21,7 +21,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
     }
 
     if (!node)
-        return -1;
+        return (-1);
 
     if (node->prev)
         node->prev->next = node->next;
@@ -32,5 +32,5 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
         node->next->prev = node->prev;
 
     free(node);
-    return 1;
+    return (1);
 }
